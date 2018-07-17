18376a51eaf5b89c6adadbe772d7f8dfcb4ddc42
Hugo W <hugwnt@gmail.com>
2018-07-17 11:33:44 -0300

    Add core functions and remove handlers

---
+++ src/__test__/core.test.js
@@ -0,0 +1,244 @@
+import ndtk from 'ndtk'
+import Joi from 'joi'
+
+import core from '@/core'
+
+describe('Expressio / Utils', () => {
+  describe('#initialize', () => {
+    const fn = jest.fn()
+
+    afterEach(() => {
+      fn.mockClear()
+    })
+
+    const server = {
+      config: {
+        test: { enabled: true },
+      },
+      initialize: core.initialize,
+    }
+
+    it('should load the initializer with proper params', () => {
+      server.initialize('test', fn)
+      expect(fn).toHaveBeenCalledWith(server, { enabled: true })
+    })
+
+    it('given a server with invalid configs, it should load the initializer with an empty object as config', () => {
+      server.initialize('misc', fn)
+      expect(fn).toHaveBeenCalledWith(server, {})
+    })
+
+    it('given an initializer with invalid name, it should throw an error with proper message', () => {
+      expect(() => server.initialize('', fn)).toThrow('Initialize error: name is not a string')
+      expect(() => server.initialize(null, fn)).toThrow('Initialize error: name is not a string')
+      expect(() => server.initialize(undefined, fn)).toThrow('Initialize error: name is not a string')
+      expect(() => server.initialize(2, fn)).toThrow('Initialize error: name is not a string')
+      expect(() => server.initialize({}, fn)).toThrow('Initialize error: name is not a string')
+    })
+
+    it('given an initializer with invalid function, it should throw an error with proper message', () => {
+      expect(() => server.initialize('test', null)).toThrow('Initialize error: "test" has not a valid function')
+      expect(() => server.initialize('test', {})).toThrow('Initialize error: "test" has not a valid function')
+      expect(() => server.initialize('test', undefined)).toThrow('Initialize error: "test" has not a valid function')
+      expect(() => server.initialize('test', 'test')).toThrow('Initialize error: "test" has not a valid function')
+      expect(() => server.initialize('test', 10)).toThrow('Initialize error: "test" has not a valid function')
+    })
+  })
+
+  describe('#controller', () => {
+    const resource = jest.fn().mockResolvedValue()
+    const badResource = jest.fn().mockRejectedValue('Ops')
+    const next = jest.fn()
+
+    afterEach(() => {
+      resource.mockClear()
+      next.mockClear()
+      badResource.mockClear()
+    })
+
+    it('should execute a valid async route', async () => {
+      const route = core.controller(resource)
+      await route(null, null, next)
+
+      expect(resource).toHaveBeenCalledWith(null, null, next)
+    })
+
+    it('should execute and catch errors thrown from a bad async route', async () => {
+      const route = core.controller(badResource)
+      await route(null, null, next)
+
+      expect(badResource).toHaveBeenCalledWith(null, null, next)
+      expect(next).toHaveBeenCalledWith('Ops')
+    })
+
+    it('given an invalid resource, it should throw an error with proper message', () => {
+      expect(() => core.controller()).toThrow('Controller error: resource is not a function')
+      expect(() => core.controller(null)).toThrow('Controller error: resource is not a function')
+      expect(() => core.controller(undefined)).toThrow('Controller error: resource is not a function')
+      expect(() => core.controller(true)).toThrow('Controller error: resource is not a function')
+      expect(() => core.controller(10)).toThrow('Controller error: resource is not a function')
+      expect(() => core.controller({})).toThrow('Controller error: resource is not a function')
+      expect(() => core.controller([])).toThrow('Controller error: resource is not a function')
+      expect(() => core.controller('string')).toThrow('Controller error: resource is not a function')
+    })
+  })
+
+  describe('#validate', () => {
+    const next = jest.fn()
+
+    afterEach(() => {
+      next.mockClear()
+    })
+
+    it('given a request with good payload, it should reassign the sanitized data to the request object', () => {
+      const schema = Joi.object({
+        foo: Joi.boolean().required(),
+        bar: Joi.string().trim().required(),
+      })
+
+      const req = {
+        body: {
+          foo: true,
+          bar: ' bar ',
+          extra: '...',
+        },
+      }
+
+      const handler = core.validate('body', schema)
+      handler(req, null, next)
+
+      expect(next).toHaveBeenCalled()
+      expect(req.body).toEqual({ foo: true, bar: 'bar' })
+    })
+
+    it('given an invalid source, it should throw an error with proper message', () => {
+      expect(() => core.validate()).toThrow('Validate error: source is not a string')
+      expect(() => core.validate(null)).toThrow('Validate error: source is not a string')
+      expect(() => core.validate(undefined)).toThrow('Validate error: source is not a string')
+      expect(() => core.validate(true)).toThrow('Validate error: source is not a string')
+      expect(() => core.validate({})).toThrow('Validate error: source is not a string')
+      expect(() => core.validate([])).toThrow('Validate error: source is not a string')
+    })
+
+    it('given an invalid schema, it should throw an error with proper message', () => {
+      expect(() => core.validate('body', {})).toThrow('Validate error: schema provided is not a valid Joi schema')
+    })
+
+    it('given a not allowed source, it should throw an error with proper message', () => {
+      expect(() => core.validate('', Joi.string().required())).toThrow('Validate error: bad validation source, possible options are "body", "params", "query"')
+    })
+
+    it('given a request with empty payloads, it should throw an http error with proper information', () => {
+      const handler = core.validate('body', Joi.string().required())
+      try {
+        handler({}, null, null)
+      } catch (err) {
+        expect(err.isHttp).toBeTruthy()
+        expect(err.message).toEqual('Request body data is missing')
+        expect(err.output).toEqual({
+          message: 'Request body data is missing',
+          status: 422,
+          type: 'VALIDATION',
+        })
+      }
+    })
+
+    it('given a request with bad payload, it should throw an http error with proper information', () => {
+      const schema = Joi.object({
+        foo: Joi.boolean().required(),
+        bar: Joi.string().required(),
+      })
+
+      const handler = core.validate('params', schema)
+      try {
+        handler({ params: {} }, null, null)
+      } catch (err) {
+        expect(err.isHttp).toBeTruthy()
+        expect(err.message).toEqual('Invalid request params data')
+        expect(err.output).toEqual({
+          message: 'Invalid request params data',
+          status: 422,
+          type: 'VALIDATION',
+          attributes: {
+            bar: {
+              message: 'bar is required',
+              type: 'any.required',
+            },
+            foo: {
+              message: 'foo is required',
+              type: 'any.required',
+            },
+          },
+        })
+      }
+    })
+  })
+
+  describe('#notFoundHandler', () => {
+    const next = jest.fn()
+
+    afterEach(() => {
+      next.mockClear()
+    })
+
+    it('should send a formatted 404 error object', () => {
+      core.notFoundHandler(null, null, next)
+      const error = next.mock.calls[0][0]
+      expect(error).toBeInstanceOf(Error)
+      expect(error.message).toEqual('Not Found')
+      expect(error.isHttp).toBeTruthy()
+      expect(error.output).toEqual({
+        message: 'Not Found',
+        status: 404,
+        type: 'NOT_FOUND',
+      })
+    })
+  })
+
+  describe('#generalErrorHandler', () => {
+    const status = jest.fn()
+    const json = jest.fn()
+    const logError = jest.fn()
+
+    const res = { status, json }
+    const req = { logger: { error: logError } }
+
+    afterEach(() => {
+      status.mockClear()
+      logError.mockClear()
+      logError.mockClear()
+    })
+
+    it('given an http error object, it should send the proper status code and error output', () => {
+      const error = ndtk.httpError(404)
+      core.generalErrorHandler(error, req, res)
+      expect(logError).toHaveBeenCalled()
+      expect(status).toHaveBeenCalledWith(404)
+      expect(json).toHaveBeenCalledWith(error.output)
+    })
+
+    it('given a non http error object, it should send the proper status code and error output', () => {
+      const error = new Error('Ops!')
+      core.generalErrorHandler(error, req, res)
+      expect(logError).toHaveBeenCalled()
+      expect(status).toHaveBeenCalledWith(500)
+      expect(json).toHaveBeenCalledWith({
+        message: 'Internal Server Error',
+        status: 500,
+        type: 'INTERNAL_SERVER_ERROR',
+      })
+    })
+
+    it('given an error object without message, it should send the proper status code and error output', () => {
+      const error = new Error()
+      core.generalErrorHandler(error, req, res)
+      expect(logError).toHaveBeenCalled()
+      expect(status).toHaveBeenCalledWith(500)
+      expect(json).toHaveBeenCalledWith({
+        message: 'Internal Server Error',
+        status: 500,
+        type: 'INTERNAL_SERVER_ERROR',
+      })
+    })
+  })
+})
---
+++ src/core.js
@@ -0,0 +1,107 @@
+/**
+ * Core
+ *
+ * @copyright Copyright (c) 2018, hugw.io
+ * @author Hugo W - contact@hugw.io
+ * @license MIT
+ */
+
+import ndtk from 'ndtk'
+import isFunction from 'lodash/isFunction'
+import isString from 'lodash/isString'
+import Joi from 'joi'
+
+/**
+ * Load initializers
+ */
+function initialize(name, fn) {
+  const server = this
+
+  ndtk.assert(isString(name) && name.length !== 0, 'Initialize error: name is not a string')
+  ndtk.assert(isFunction(fn), `Initialize error: "${name}" has not a valid function`)
+
+  // If no valid config is found, then return
+  // an empty object as the second arg
+  fn(server, server.config[name] || {})
+}
+
+/**
+ * Execute body/params/query validations
+ * and throw formatted error objects for possible errors
+ * or append sanitized data to the request object
+ */
+const validate = (source, schema) => {
+  ndtk.assert(isString(source), 'Validate error: source is not a string')
+
+  // Ensure schema is a valid Joi object
+  ndtk.assert(schema && schema.isJoi, 'Validate error: schema provided is not a valid Joi schema')
+
+  const validSource = ['body', 'params', 'query'].includes(source)
+  ndtk.assert(validSource, 'Validate error: bad validation source, possible options are "body", "params", "query"')
+
+  return (req, res, next) => {
+    // First check for empty payloads
+    if (!req[source]) throw ndtk.httpError(422, { message: `Request ${source} data is missing`, type: 'VALIDATION' })
+
+    const result = Joi.validate(req[source], schema, { stripUnknown: true, abortEarly: false })
+
+    if (result.error) {
+      const { details } = result.error
+
+      const attributes = details.reduce((obj, validation) => {
+        const { context: { key }, message, type } = validation
+        return Object.assign({}, obj, { [key]: { message: message.replace(/"/g, ''), type } })
+      }, {})
+
+      throw ndtk.httpError(422, { message: `Invalid request ${source} data`, type: 'VALIDATION', attributes })
+    }
+
+    // Reassign sanitized data back
+    // to the request object ( with unknown keys removed )
+    req[source] = result.value
+    next()
+  }
+}
+
+/**
+ * Controller middleware
+ * to handle async errors
+ */
+const controller = (resource) => {
+  ndtk.assert(isFunction(resource), 'Controller error: resource is not a function')
+
+  return async (req, res, next) => {
+    try {
+      await resource(req, res, next)
+    } catch (e) { next(e) }
+  }
+}
+
+/**
+ * Format all caught errors
+ * and return an http error object
+ */
+const generalErrorHandler = (err, req, res, next) => { // eslint-disable-line
+  // For the purpose of logging at least a message
+  err.message = err.message || 'Something bad happened'
+
+  req.logger.error(err)
+  const { output } = err.isHttp ? err : ndtk.httpError()
+
+  res.status(output.status)
+  res.json({ ...output })
+}
+
+/**
+ * Format not found routes
+ * to return a 404 response
+ */
+const notFoundHandler = (req, res, next) => next(ndtk.httpError(404))
+
+export default {
+  initialize,
+  controller,
+  generalErrorHandler,
+  notFoundHandler,
+  validate,
+}
